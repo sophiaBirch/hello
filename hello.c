e3e4072fc2cf5517ac0fce06e4c53e86e92de0eb
LKEWXD <lwooten5853@gmail.com>
2022-08-23 19:41:08 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Luke Wooten
+ *         lwooten@unomaha.edu
+ * Computer Engineering
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Luke Wooten\n");
+
+  return 0;
+}
