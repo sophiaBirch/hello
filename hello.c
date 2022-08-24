1fd7782ca5a3802c3ece814b4513ca1ae4d8c48f
Sophia Birch <Sophia Birch>
2022-08-24 17:28:25 +0000

    added points

--- hello.c
+++ hello.c
@@ -4,5 +4,13 @@
  * 
  * 2022/08/23
  */
+ #include <stdio.h>
+ #include <stdlib.h>
  
- 
+ int main(int argc, char **argv){
+     
+     printf("Hello World!!!!\n");
+     printf("Sophia Birch\n");
+     
+     
+ }
